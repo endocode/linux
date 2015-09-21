053cf94f4916b6290798444f9b7792cd7fb4a00a
Dongsu Park <dongsu@endocode.com>
2015-09-21 15:16:21 +0200

    fs/9p: adjust prototype of generic_fillattr()
    adjust generic_fillattr() to avoid compile error.

--- fs/9p/vfs_inode.c
+++ fs/9p/vfs_inode.c
@@ -1061,7 +1061,7 @@ v9fs_vfs_getattr(struct vfsmount *mnt, struct dentry *dentry,
 	p9_debug(P9_DEBUG_VFS, "dentry: %p\n", dentry);
 	v9ses = v9fs_dentry2v9ses(dentry);
 	if (v9ses->cache == CACHE_LOOSE || v9ses->cache == CACHE_FSCACHE) {
-		generic_fillattr(d_inode(dentry), stat);
+		generic_fillattr(mnt, d_inode(dentry), stat);
 		return 0;
 	}
 	fid = v9fs_fid_lookup(dentry);
@@ -1073,7 +1073,7 @@ v9fs_vfs_getattr(struct vfsmount *mnt, struct dentry *dentry,
 		return PTR_ERR(st);
 
 	v9fs_stat2inode(st, d_inode(dentry), d_inode(dentry)->i_sb);
-	generic_fillattr(d_inode(dentry), stat);
+	generic_fillattr(mnt, d_inode(dentry), stat);
 
 	p9stat_free(st);
 	kfree(st);
